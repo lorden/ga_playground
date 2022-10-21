d1f6a8dcb4a99f9b1d2a41be99707620d9390e7e
Diego Lorden <diego@endwaste.io>
2022-10-21 16:22:30 -0700

    clean edit

--- .github/workflows/clean_release_notes.py
+++ .github/workflows/clean_release_notes.py
@@ -4,7 +4,17 @@ import re
 import sys
 
 def clean_notes(notes):
+  # Remove comments
   clean_notes = re.sub("(<!--.*?-->)", "", notes, flags=re.DOTALL)
+
+  # Change h2 titles
+  clean_notes = re.sub('##(.*)\r\n', r'*\1*', clean_notes)
+
+  # Change h3 titles
+  clean_notes = re.sub('###(.*)\r\n', r'*_\1_*', clean_notes)
+
+  # Change bullet points
+  clean_notes = re.sub('\n\* (.*)\r\n', r'• \1', clean_notes)
 
   env_file = os.getenv('GITHUB_ENV')
   with open(env_file, 'a') as ef:
