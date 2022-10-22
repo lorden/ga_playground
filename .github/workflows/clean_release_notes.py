cb4e7b7f11841f569b2198520d5c54dde018c7e9
Diego Lorden <diego@endwaste.io>
2022-10-21 17:24:35 -0700

    Fix

--- .github/workflows/clean_release_notes.py
+++ .github/workflows/clean_release_notes.py
@@ -8,17 +8,21 @@ def clean_notes(notes):
   clean_notes = re.sub("(<!--.*?-->)", "", notes, flags=re.DOTALL)
 
   # Change h2 titles
-  clean_notes = re.sub('##(.*)\r\n', r'*\1*', clean_notes)
+  clean_notes = re.sub('\n## (.*?)\n', r'\n*\1*\n', clean_notes)
 
   # Change h3 titles
-  clean_notes = re.sub('###(.*)\r\n', r'*_\1_*', clean_notes)
+  clean_notes = re.sub('\n### (.*?)\n', r'\n*_\1_*\n', clean_notes)
 
   # Change bullet points
-  clean_notes = re.sub('\n\* (.*)\r\n', r'• \1', clean_notes)
+  clean_notes = re.sub('\n\* (.*?)\n', r'\n• \1\n', clean_notes, flags=re.DOTALL)
 
+  return clean_notes
+
+def set_env_var(name, content):
   env_file = os.getenv('GITHUB_ENV')
   with open(env_file, 'a') as ef:
-    ef.write(f'RELEASE_NOTES={clean_notes}')
+    ef.write(f'{name}={content}')
 
 if __name__ == '__main__':
-  clean_notes(sys.argv[1])
+  notes = clean_notes(sys.argv[1])
+  set_env_var('RELEASE_NOTES', notes)
